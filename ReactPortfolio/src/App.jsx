0b539e6109137c331a0fbe46b2a1653344a45339
DevonRem <DevonReminick@gmail.com>
2023-12-18 21:29:26 -0500

    Created Header component, header css

---
+++ ReactPortfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <Header></Header>
+
+    </>
+  )
+}
+
+export default App
---
+++ ReactPortfolio/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import './Header.css';
+
+function Header() {
+
+    return (
+    <>
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="navLogo">Devon Reminick</a>
+
+                <div className="navMenu">
+                    <ul className="navList">
+                        <li className="navItem">
+                            <a href="#home" className="navLink activeLink">
+                                <i className="uil uil-estate navIcon"></i> Home
+                            </a>
+                        </li>
+
+                        <li className="navItem">
+                            <a href="#projects" className="navLink">
+                                <i className="uil uil-scenery navIcon"></i> Projects
+                            </a>
+                        </li>
+
+                        <li className="navItem">
+                            <a href="#skills" className="navLink">
+                                <i className="uil uil-file-alt navIcon"></i> Skills
+                            </a>
+                        </li>
+
+                        <li className="navItem">
+                            <a href="#about" className="navLink">
+                                <i className="uil uil-user navIcon"></i> About
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i className="uil uil-times navClose"></i>
+                </div>
+                
+                <div className='navToggle'>
+                    <i className="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
+    </>
+    )
+}
+
+export default Header
