fbfccd27f50b504d4587f604656e6008a1ea2128
DevonRem <DevonReminick@gmail.com>
2023-12-30 15:38:51 -0500

    Started work on Project section, first project outline added

--- ReactPortfolio/src/App.jsx
+++ ReactPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
 import Skills from './components/Skills.jsx'
 import About from './components/About.jsx';
+import Projects from './components/Projects.jsx';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
     <Header></Header>
     <Home></Home>
+    <Projects></Projects>
     <Skills></Skills>
     <About></About>
 
--- ReactPortfolio/src/components/Projects.jsx
+++ ReactPortfolio/src/components/Projects.jsx
@@ -1,3 +1,5 @@
+import "./Projects.css";
+import RSCImg from "../assets/RSCImg.png";
 
 function Projects() {
 
@@ -5,7 +7,25 @@ function Projects() {
     return (
       <>
   
-        <div>Projects A</div>
+        <div className="projects" id="projects">
+          <h2 className="sectionTitle">Projects:</h2>
+          <div className="sectionSubtitle">Recent work:</div>
+
+          <div className="projectBox container grid">
+            <div className="projectTopBox">
+              <h3 className="projectTitle">React Shopping Cart</h3>
+              <a href=" https://github.com/DevonRem/ReactShoppingCart" className="button buttonFlex">Github</a>
+              <a href="https://react-shopping-cart-opal.vercel.app/" className="button buttonFlex">Live</a>
+              <img src={RSCImg} className="projectImg"></img>
+            </div>
+            <div className="projectBottomBox">
+              <div className="toolsUsed">Tools used:HTML, CSS, JavaScript, React</div>
+              <div className="description">Description: </div>
+            </div>
+            
+          </div>
+
+        </div>
   
       </>
     )
